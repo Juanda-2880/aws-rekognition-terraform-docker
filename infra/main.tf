25282df867152648fffbdcec281fbf46c07e8da1
Juan David Pacheco <juandapv995@gmail.com>
2026-01-21 20:29:16 -0500

    Add Dockerfile and IAM resources for Rekognition app
    Added a Dockerfile for the Python app and renamed requirements file for correct spelling. Updated Terraform configuration to create IAM user, policy, access keys, and attach permissions for S3 and Rekognition. Outputs for access and secret keys were also added.

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,47 @@
 resource "aws_s3_bucket" "bucketforrekognition" {
     bucket = "bucket-for-rekognition-project-2880"
-    region = var.region
     tags = {
       Name = "My Bucket Project"
       Environment = "Dev"
     }
+}
+
+resource "aws_iam_user" "app_user" {
+    name = "RecognitionAppUser"
+}
+
+resource "aws_iam_policy" "app_policy" {
+    name = "RecognitionAppPolicy"
+    description = "Policy for Rekognition App User"
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Action = [
+                    "s3:GetObject",
+                    "s3:PutObject"
+                ]
+                Effect   = "Allow"
+                Resource = "${aws_s3_bucket.bucketforrekognition.arn}/*"
+            },
+            {
+                Action = [
+                    "rekognition:DetectLabels",
+                    "rekognition:IndexFaces",
+                    "rekognition:SearchFacesByImage"
+                ]
+                Effect   = "Allow"
+                Resource = "*"
+            }
+        ]
+    })
+}
+
+resource "aws_iam_user_policy_attachment" "app_user_policy_attach" {
+    user       = aws_iam_user.app_user.name
+    policy_arn = aws_iam_policy.app_policy.arn
+}
+
+resource "aws_iam_access_key" "app_user_access_key" {
+    user = aws_iam_user.app_user.name
 }
--- infra/output.tf
+++ infra/output.tf
@@ -2,3 +2,14 @@ output "bucket-for-rekognition-project-2880" {
     description = "Name of the S3 Bucket for Rekognition Project"
     value       = aws_s3_bucket.bucketforrekognition.bucket
 }
+
+output "app_access_key" {
+  description = "Access Key del usuario de la App"
+  value       = aws_iam_access_key.app_user_access_key.id
+}
+
+output "app_secret_key" {
+  description = "Secret Key del usuario de la App"
+  value       = aws_iam_access_key.app_user_access_key.secret
+  sensitive   = true  
+}
